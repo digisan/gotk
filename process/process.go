9b137cc81b29be415292111ddbf5d807d959f033
Qing Miao <digisanQM@outlook.com>
2021-04-22 10:05:24 +1000

    * fix GetRunningPID 15+len process-name

--- process/process.go
+++ process/process.go
@@ -4,6 +4,8 @@ import (
 	"log"
 	"os/exec"
 	"path/filepath"
+
+	"github.com/digisan/gotk/io"
 )
 
 // GetRunningPID:
@@ -14,7 +16,9 @@ func GetRunningPID(pathOfExe string) (pidGrp []string) {
 	}
 
 	dir, exe := filepath.Dir(abspath), filepath.Base(abspath)
-	out, err := exec.Command("/bin/sh", "-c", "pgrep "+exe).CombinedOutput()
+
+	// ps -Af | grep ***
+	out, err := exec.Command("/bin/sh", "-c", "ps -Af | grep "+exe).CombinedOutput()
 	if fSf("%v", err) == "exit status 1" {
 		return
 	}
@@ -22,8 +26,22 @@ func GetRunningPID(pathOfExe string) (pidGrp []string) {
 		log.Fatalf("%v", err)
 	}
 
-	outstr := sTrim(string(out), " \t\r\n")
-	for _, pid := range sSplit(outstr, "\n") {
+	pidGrpTemp := []string{}
+	io.StrLineScan(string(out), func(ln string) (bool, string) {
+		I := 0
+		for _, seg := range sSplit(ln, " ") {
+			if len(seg) > 0 {
+				I++
+			}
+			if I == 2 {
+				pidGrpTemp = append(pidGrpTemp, seg)
+				break
+			}
+		}
+		return true, ""
+	}, "")
+
+	for _, pid := range pidGrpTemp {
 		out, err := exec.Command("/bin/sh", "-c", "pwdx "+pid).CombinedOutput()
 		if fSf("%v", err) == "exit status 1" {
 			return
@@ -32,12 +50,40 @@ func GetRunningPID(pathOfExe string) (pidGrp []string) {
 			log.Fatalf("%v", err)
 		}
 
-		outstr := sTrim(string(out), " \t\r\n")
-		procpath := sSplit(outstr, ": ")[1]
+		procpath := sSplit(sTrim(string(out), " \t\r\n"), ": ")[1]
 		if dir == procpath {
 			pidGrp = append(pidGrp, pid)
 		}
 	}
+
+	//
+
+	// dir, exe := filepath.Dir(abspath), filepath.Base(abspath)
+	// out, err := exec.Command("/bin/sh", "-c", "pgrep "+exe).CombinedOutput()
+	// if fSf("%v", err) == "exit status 1" {
+	// 	return
+	// }
+	// if err != nil {
+	// 	log.Fatalf("%v", err)
+	// }
+
+	// outstr := sTrim(string(out), " \t\r\n")
+	// for _, pid := range sSplit(outstr, "\n") {
+	// 	out, err := exec.Command("/bin/sh", "-c", "pwdx "+pid).CombinedOutput()
+	// 	if fSf("%v", err) == "exit status 1" {
+	// 		return
+	// 	}
+	// 	if err != nil {
+	// 		log.Fatalf("%v", err)
+	// 	}
+
+	// 	outstr := sTrim(string(out), " \t\r\n")
+	// 	procpath := sSplit(outstr, ": ")[1]
+	// 	if dir == procpath {
+	// 		pidGrp = append(pidGrp, pid)
+	// 	}
+	// }
+
 	return
 }
 
--- process/process_test.go
+++ process/process_test.go
@@ -1,1 +1,12 @@
 package process
+
+import (
+	"fmt"
+	"testing"
+	"time"
+)
+
+func TestGetRunningPID(t *testing.T) {
+	fmt.Println(GetRunningPID("/home/qmiao/Desktop/n3/nats-streaming-server-v0.21.2-linux-amd64/nats-streaming-server"))
+	fmt.Println(time.Now())
+}
