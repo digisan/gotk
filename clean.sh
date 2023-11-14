bfc14dc37b1f8dfc33058c7583a334debcb3abb3
Qing Miao <cdutwhu@outlook.com>
2023-11-14 20:51:17 +1100

    * update mod

--- clean.sh
+++ clean.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-rm -f ./io/*.txt
-
-rm -f ./crypto/*.pem
-
 # delete all binary files
 find . -type f -executable -exec sh -c "file -i '{}' | grep -q 'x-executable; charset=binary'" \; -print | xargs rm -f
-for f in $(find ./ -name '*.log' -or -name '*.txt'); do rm $f; done
+for f in $(find ./ -name '*.log' -or -name '*.txt'); do rm -rf $f; done
 find ./ -type d -empty -delete
--- go.mod
+++ go.mod
@@ -4,8 +4,8 @@ go 1.21
 
 require (
 	github.com/BurntSushi/toml v1.3.2
-	github.com/digisan/go-generics v0.4.1
+	github.com/digisan/go-generics v0.4.2
 	github.com/h2non/filetype v1.1.3
-	golang.org/x/net v0.17.0
+	golang.org/x/net v0.18.0
 	gopkg.in/yaml.v3 v3.0.1
 )
